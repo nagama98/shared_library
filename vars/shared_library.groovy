aa34ebd435ef942c2769eea83e129b49eee556d6
nagama98 <42217140+nagama98@users.noreply.github.com>
2021-04-21 14:48:01 +0530

    Update shared_library.groovy

--- vars/shared_library.groovy
+++ vars/shared_library.groovy
@@ -20,3 +20,33 @@ responseText = slurper.parseText(responseText)
      
 println(responseText)
 }
+
+
+//create quality gate
+
+def create(def name)
+{
+
+def conn = null
+
+conn = new URL("http://localhost:9000/api/qualitygates/create?name="+name+"").openConnection();
+conn.setRequestMethod("POST")
+conn.setRequestProperty("Authorization", "Basic YWRtaW46YWRtaW4x")
+conn.setDoOutput(true)
+        
+OutputStream os = conn.getOutputStream();
+//os.write(body.getBytes("UTF-8"));
+os.write();
+os.flush();
+os.close();
+
+
+def bufferedReader = new BufferedReader(new InputStreamReader( conn.getInputStream()))
+    
+def slurper = new JsonSlurper()
+def resultMap = slurper.parseText(bufferedReader.getText())
+
+println(resultMap)
+
+     
+}
